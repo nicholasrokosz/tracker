611dce71defc361155ea9ed49d61ae6db1700dde
Nick Rokosz <nicholasrokosz@gmail.com>
2024-07-02 12:56:46 -0500

    Add recharts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+"use client";
 import { Button } from "~/components/ui/button";
+import { LineChart, Line } from "recharts";
 
 export default function HomePage() {
+  const data = [
+    { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
+    { name: "Page B", uv: 500, pv: 2400, amt: 2400 },
+    { name: "Page C", uv: 300, pv: 2400, amt: 2400 },
+  ];
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <Button>Click me</Button>
+      <LineChart width={400} height={400} data={data}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      </LineChart>
     </main>
   );
 }
